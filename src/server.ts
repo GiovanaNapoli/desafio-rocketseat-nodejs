9650b2578be6385eb2e43403d77f35c4a252dfea
GiovanaNapoli <giovana.ap.napoli@outlook.com>
2025-08-31 11:49:11 -0300

    feat: adicionando novas rotas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import crypto from "node:crypto";
 import { db } from "./db/client.ts";
 import { courses } from "./db/schema.ts";
+import { eq } from "drizzle-orm";
 
 const server = fastify({
   logger: {
@@ -16,44 +17,49 @@ const server = fastify({
   },
 });
 
-
 server.get("/courses", async (request, reply) => {
   const result = await db.select().from(courses);
 
   return reply.send({ courses: result });
 });
 
-// server.get("/courses/:id", (request, reply) => {
-//   type Params = {
-//     id: string;
-//   };
+server.get("/course/:id", async (request, reply) => {
+  type Params = {
+    id: string;
+  };
+  const params = request.params as Params;
+  const courseId = params.id;
 
-//   const params = request.params as Params;
-//   const courseId = params.id;
+  const course = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
 
-//   const course = courses.find((c) => c.id === courseId);
+  if (course) return reply.send({ course });
 
-//   if (course) return { course };
+  return reply.status(404).send();
+});
 
-//   return reply.status(404).send({ error: "Course not found" });
-// });
+server.post("/courses", async (request, reply) => {
+  type Body = {
+    title: string;
+  };
 
-// server.post("/courses", (request, reply) => {
-//   type Body = {
-//     title: string;
-//   };
+  const body = request.body as Body;
+  const corseTitle = body.title;
 
-//   const body = request.body as Body;
-//   const corseTitle = body.title;
+  if (!corseTitle)
+    return reply.status(400).send({ error: "Course title is required" });
 
-//   if (!corseTitle)
-//     return reply.status(400).send({ error: "Course title is required" });
+  const result = await db
+    .insert(courses)
+    .values({
+      title: corseTitle,
+    })
+    .returning();
 
-//   const curseId = crypto.randomUUID();
-//   courses.push({ id: curseId, title: corseTitle });
-
-//   reply.code(201).send({ id: curseId });
-// });
+  reply.code(201).send({ id: result[0].id });
+});
 
 server.listen({ port: 3000 }).then(() => {
   console.log("Server is running on http://localhost:3000");
